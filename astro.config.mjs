ac2f2f054567d2abdcf5384c0a225848a0b369df
Jefferson <stupidjeff430@gmail.com>
2024-11-21 15:32:51 +0700

    fixed maybe?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,19 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import solidJs from '@astrojs/solid-js'
-import sitemap from '@astrojs/sitemap'
-import robots from 'astro-robots'
-import vercel from '@astrojs/vercel'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
+import robots from 'astro-robots';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hi.jeffersonrj.com',
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   integrations: [solidJs(), tailwind(), sitemap(), robots()],
-  // Change 'hybrid' to 'static' for static site generation
-  output: 'static',
-  // Add the Vercel adapter to deploy on Vercel
-  adapter: vercel(),
-  image: {
-    service: 'none'
-  },
   vite: {
     esbuild: {
-      target: 'es2022'
-    }
-  }
-})
+      target: 'es2022', // Set to modern JavaScript
+    },
+  },
+});
